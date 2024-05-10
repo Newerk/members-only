ca228189c84c3405f9c229f2eb6e4df55ebe8956
newerk <juniusowens@yahoo.com>
2024-05-10 16:37:08 -0400

    Add Basic CSS Styling for Header on Layout.pug

--- routes/home.js
+++ routes/home.js
@@ -1,9 +1,9 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('home', { title: 'Express' });
+router.get("/", function (req, res, next) {
+  res.render("home");
 });
 
 module.exports = router;
