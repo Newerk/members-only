b2d9451abf90e9a7ab36408b3c2d7089ac4d3ec3
newerk <juniusowens@yahoo.com>
2024-06-03 15:29:19 -0400

    Redirect to Login Page Instead of Rendering After User Signs Up

--- controllers/signupController.js
+++ controllers/signupController.js
@@ -8,22 +8,20 @@ module.exports = {
   }),
 
   signup_post: asyncHandler(async (req, res, next) => {
-
     bcrypt.hash(req.body.password, 10, async (err, hashedPassword) => {
-        // if err, do something
-        // otherwise, store hashedPassword in DB
-        const newUser = new Account({
-            firstname: req.body.firstname,
-            lastname: req.body.lastname,
-            username: req.body.username,
-            password: hashedPassword/*generatePassword(hash, */ /*req.body.password*/ /*, salt)*/,
-          });
-      
-          await newUser.save();
-      
-          res.render("home");
-      
+      // if err, do something
+      // otherwise, store hashedPassword in DB
+      const newUser = new Account({
+        firstname: req.body.firstname,
+        lastname: req.body.lastname,
+        username: req.body.username,
+        password:
+          hashedPassword /*generatePassword(hash, */ /*req.body.password*/ /*, salt)*/,
       });
-      
+
+      await newUser.save();
+
+      res.redirect("login");
+    });
   }),
 };
